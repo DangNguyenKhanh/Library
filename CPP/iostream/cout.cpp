da06c996243c4cdefd9441c93bcb29786d6efa5a
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-07 01:05:16 +0700

    Create cout.cpp

---
+++ CPP/iostream/cout.cpp
@@ -0,0 +1,13 @@
+Độ rộng là 10 và canh lề trái:
+  #include<iostream>
+  using namespace std;
+  int main()
+  {
+      cout.width(10);
+      cout << left;
+      cout << "ID";
+
+      cout.width(10);
+      cout << left;
+      cout << "Name";
+  }
