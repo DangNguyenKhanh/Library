4da64fb3adad2b5b9b48f9d16e82b983727d7ead
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-07 00:37:06 +0700

    Update getline.cpp

--- CPP/string/getline.cpp
+++ CPP/string/getline.cpp
@@ -1,6 +1,6 @@
 Sử dụng chuỗi string dể lấy kí tự trắng
   #include<iostream>
-  #include<string>
+  #include<string>  // khai báo để sử dụng string và getline()
   using namespace std;
   int main()
   {
