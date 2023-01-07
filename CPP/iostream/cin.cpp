568acc52165e12052c283725a88f3c714e062a18
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-07 14:55:58 +0700

    Update cin.cpp

--- CPP/iostream/cin.cpp
+++ CPP/iostream/cin.cpp
@@ -11,10 +11,11 @@ Nhập dữ liệu bằng đối tượng cin:
 Sử dụng cin.clear() xóa bỏ trạng thái lỗi:
 lấy dữ liệu từ biểu thức toán học: 
   Example: 3 * 4  => int a = 3, int b = 4, char symbol = '*'
+  Code C++:
     int a, b;
     char symbol;
-    cin >> a;
-    cin.clear();
-    cin >> symbol;
-    cin.clear();
-    cin >> b;
+    cin >> a;       // a = 3
+    cin.clear();    // giữ lại input stream không xóa,          input stream = " * 4"
+    cin >> symbol;  // bỏ qua kí tự trắng để symbol = '*',      input stream = " 4"
+    cin.clear();    // giữ lại input stream không xóa,          input stream = " 4"
+    cin >> b;       // bỏ qua kí tự trắng để b = 4,             input stream = ""
