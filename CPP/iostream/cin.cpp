06837c4f060c29181017df6fcacccf42ac1b6186
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-07 14:52:16 +0700

    Update cin.cpp

--- CPP/iostream/cin.cpp
+++ CPP/iostream/cin.cpp
@@ -1,3 +1,12 @@
+######################################################################################################
+Nhập dữ liệu bằng đối tượng cin:
+    int a;
+    cin >> a;
+- cin sẽ bỏ đi kí tự khoảng trắng cho đến khi gặp được kí số: 
+    Example:  "           10"   =>    a = 10
+- cin sẽ ngắt số nếu gặp khoảng trắng:
+    Example:  "100 200"   =>    a = 100
+######################################################################################################
 Sử dụng cin.clear() xóa bỏ trạng thái lỗi:
 lấy dữ liệu từ biểu thức toán học: 
   Example: 3 * 4  => int a = 3, int b = 4, char symbol = '*'
