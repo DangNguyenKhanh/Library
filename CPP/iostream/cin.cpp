9899d54a518b22b9face76e8156722258eb7ae80
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-07 14:45:03 +0700

    Create cin.cpp

---
+++ CPP/iostream/cin.cpp
@@ -0,0 +1,10 @@
+Sử dụng cin.clear() xóa bỏ trạng thái lỗi:
+lấy dữ liệu từ biểu thức toán học: 
+  Example: 3 * 4  => int a = 3, int b = 4, char symbol = '*'
+    int a, b;
+    char symbol;
+    cin >> a;
+    cin.clear();
+    cin >> symbol;
+    cin.clear();
+    cin >> b;
