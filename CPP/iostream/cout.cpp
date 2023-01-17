a6d2924928308befb2652aecbaa8fa54778213e2
Call me Khanh <121375975+DangNguyenKhanh@users.noreply.github.com>
2023-01-17 14:25:15 +0700

    Update cout.cpp

--- CPP/iostream/cout.cpp
+++ CPP/iostream/cout.cpp
@@ -1,4 +1,8 @@
-Độ rộng là 10 và canh lề trái:
+cout là đối tượng của ostream
+
+------------------------------------------------------------------------
+Qua định độ rộng: cout.width(int n = 0) Thiết lập độ rộng, trả về độ rộng trước đó, áp dùng cho 1 lần xuất 
+1. Độ rộng là 10 và canh lề trái:
   #include<iostream>
   using namespace std;
   int main()
@@ -11,3 +15,31 @@
       cout << left;
       cout << "Name";
   }
+
+2. Hàm quy định độ rộng
+  <iomanip>: cout << setw(int n) << a;
+
+------------------------------------------------------------------------
+Kí tự độn (ios): cout.fill(char c = ‘ ’)
+
+------------------------------------------------------------------------
+Độ chính xác – giới hạn chữ số: cout.precision(int n);
+
+1. int cout.precision(): Trả về độ chính xác hiện tại
+
+2. int cout.precision(int n); Thiết lập độ chính xác, trả về độ chính xác trước đó, hiệu lực 
+cho tới khi gặp một câu lệnh thiết lập độ chính xác mớ, ngoài ra còn làm tròn 
+
+3. Hàm setprecision <iomanip>: cout << setprecision(int n) << a;
+
+4. Từ sau dấu phẩy sẽ cố định theo setprecision: cout.setf(ios::fixed);
+
+------------------------------------------------------------------------
+Căn lề 
+1. cout.setf(ios::right)	: Căn lề phải, trái bị dư
+
+2. cout.setf(ios::left)	: Căn lề trái,  phải bị dư
+
+
+
+
